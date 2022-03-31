84caeb2ab5c3aac84286f943f2ebce5898f327b6
lborgesc <lborgescaetano@gmail.com>
2022-03-31 16:56:03 -0300

    inserindo localstorage

--- js/app.js
+++ js/app.js
@@ -1,44 +1,83 @@
-const inputTarefa = document.querySelector("input[type='text']")
-const divTodoList = document.querySelector('.todo__list')
 
-function criaTodoList(){
+function funcaoContrutora() {
     
-    window.addEventListener('keyup', e =>{
-        
-        
-        if(e.key === 'Enter'){
-
-            divTodoList.innerHTML += `
-                <label class="todo__item">
-                    <input type="checkbox">
-                    <div>${inputTarefa.value}</div>
-                    <input type="button" value="X">
-                </label>
-            `
-            limpaInput()
-            apagarTarefa()
-        }
-        
-        
-    })
-}
-
-function limpaInput() {
+    const inputTarefa = document.querySelector("input[type='text']");
+    const divTodoList = document.querySelector('.todo__list')
+    const arrayObj = JSON.parse(localStorage.getItem("Tarefa")) || []
     
-    return inputTarefa.value = ''
-}
+    arrayObj.forEach((element, index) =>  {
+        
+        element.id = index;
+        criaElemento(element)
+    });
 
-criaTodoList();  
 
-function apagarTarefa() { 
+    capturaTarefa()
+    apagarTarefa()
 
+    function capturaTarefa(){
 
-    const arrayButton = document.querySelectorAll("input[type='button']")
+        window.addEventListener('keyup', e => {
 
-    arrayButton.forEach(button => {
-        button.addEventListener('click', () => {
-            button.parentNode.remove();
+            e.preventDefault();
+
+            if (e.key === 'Enter' && inputTarefa.value !== '') {
+
+                const obj = {'tarefa': inputTarefa.value};
+                criaObj(obj);
+                limpaCampo();
+                apagarTarefa();
+            }
         })
-    })
+    }
 
-}
+    function criaObj(tarefa) {
+       
+        criaElemento(tarefa)
+        arrayObj.push(tarefa);
+        localStorage.setItem("Tarefa", JSON.stringify(arrayObj));
+    }
+
+    function criaElemento(element) {
+
+        divTodoList.innerHTML += `
+        <label class="todo__item" id='${element.id}'>
+            <input type="checkbox">
+            <div>${element.tarefa}</div>
+            <input type="button" value="X">
+        </label>
+    `
+    } 
+
+    function apagarTarefa() { 
+
+        const arrayButton = document.querySelectorAll("input[type='button']")
+        
+        arrayButton.forEach(button => {
+
+            button.addEventListener('click', ( ) => {
+                
+                
+                const itemExluido = button.parentNode;
+                const id = +itemExluido.getAttribute('id');
+
+                arrayObj.splice(arrayObj.findIndex(element => element.id === id), 1)   
+                itemExluido.remove();  
+                
+                localStorage.setItem("Tarefa", JSON.stringify(arrayObj));
+            })
+
+        })
+
+
+    
+    }    
+
+    function limpaCampo(){
+
+        return inputTarefa.value = '';
+    }
+};
+
+funcaoContrutora();
+
