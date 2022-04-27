d8a9b7c86f6695b988ad6db6921ad8f054ecbc02
lborgesc <lborgescaetano@gmail.com>
2022-04-27 15:26:09 -0300

    code refactoring

--- js/app.js
+++ js/app.js
@@ -1,83 +1,71 @@
+const getDatabase = () => JSON.parse(localStorage.getItem('todoList')) || [];
+const setDatabase = ( database ) => localStorage.setItem('todoList', JSON.stringify(database));
 
-function funcaoContrutora() {
-    
-    const inputTarefa = document.querySelector("input[type='text']");
-    const divTodoList = document.querySelector('.todo__list')
-    const arrayObj = JSON.parse(localStorage.getItem("Tarefa")) || []
-    
-    arrayObj.forEach((element, index) =>  {
-        
-        element.id = index;
-        criaElemento(element)
-    });
-
-
-    capturaTarefa()
-    apagarTarefa()
-
-    function capturaTarefa(){
-
-        window.addEventListener('keyup', e => {
-
-            e.preventDefault();
-
-            if (e.key === 'Enter' && inputTarefa.value !== '') {
-
-                const obj = {'tarefa': inputTarefa.value};
-                criaObj(obj);
-                limpaCampo();
-                apagarTarefa();
-            }
-        })
-    }
-
-    function criaObj(tarefa) {
-       
-        criaElemento(tarefa)
-        arrayObj.push(tarefa);
-        localStorage.setItem("Tarefa", JSON.stringify(arrayObj));
-    }
-
-    function criaElemento(element) {
-
-        divTodoList.innerHTML += `
-        <label class="todo__item" id='${element.id}'>
-            <input type="checkbox">
-            <div>${element.tarefa}</div>
-            <input type="button" value="X">
-        </label>
+const createItem = ( text, status, index ) => {
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = `
+        <input type="checkbox" ${status} data-index = ${index}>
+        <div>${text}</div>
+        <input type="button" value="X" data-index = ${index}>
     `
-    } 
+    document.getElementById('todoList').appendChild(item);
+}
 
-    function apagarTarefa() { 
-
-        const arrayButton = document.querySelectorAll("input[type='button']")
-        
-        arrayButton.forEach(button => {
-
-            button.addEventListener('click', ( ) => {
-                
-                
-                const itemExluido = button.parentNode;
-                const id = +itemExluido.getAttribute('id');
-
-                arrayObj.splice(arrayObj.findIndex(element => element.id === id), 1)   
-                itemExluido.remove();  
-                
-                localStorage.setItem("Tarefa", JSON.stringify(arrayObj));
-            })
-
-        })
-
-
-    
-    }    
-
-    function limpaCampo(){
-
-        return inputTarefa.value = '';
+const clearTask = ( ) => {
+    const todoList = document.getElementById('todoList');
+    while (todoList.firstChild){
+        todoList.removeChild(todoList.lastChild);
     }
-};
+}
+ 
+const renderScreen = ( ) => {
+    clearTask(); 
+    const database = getDatabase();
+    database.forEach((data, index) => createItem(data.tarefa, data.status, index));
+}
 
-funcaoContrutora();
 
+const insertItem = ( event ) => {
+    const key = event.key;
+    const text = event.target.value;
+
+    if (key === 'Enter') {
+        const database = getDatabase();
+        database.push({tarefa: text, status: " "});
+        setDatabase(database);
+        renderScreen();
+        event.target.value = '';
+    }
+}
+
+const removeItem = ( index ) => {
+    const database = getDatabase();
+    database.splice(index, 1);
+    setDatabase(database);
+    renderScreen();
+}
+
+const updateItem = ( index ) => {
+    const database = getDatabase();
+    database[index].status = database[index].status === '' ? 'checked' : '';
+    setDatabase(database);
+    renderScreen();
+}
+
+const clickItem = ( event ) => {
+    const element = event.target;
+    
+    if (element.type === 'button') {
+        const index = element.dataset.index;
+        removeItem(index)
+    }else if(element.type === 'checkbox'){
+        const index = element.dataset.index;
+        updateItem(index)
+    }  
+}
+ 
+document.getElementById('newItem').addEventListener('keypress', insertItem);
+document.getElementById('todoList').addEventListener('click', clickItem);
+
+renderScreen()
